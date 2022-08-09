419a80d69a915221d6b49c3adca44aba84b96fc5
Samara Pereira <samarapds26@gmail.com>
2022-08-09 12:35:57 -0300

    @isamarx [Task: contextos e modelos adicionados] {09/08/2022 12:35}

---
+++ EcoFarmAPI/Src/Modelos/Carrinho.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EcoFarmAPI.Src.Modelos
+{
+    /// <summary>
+    /// <para>Resumo: Classe responsavel por representar tb_carrinho no banco.</para>
+    /// <para>Criado por: Samara</para>
+    /// <para>Versão: 1.0</para>
+    /// <para>Data: 09/08/2022</para>
+    /// </summary>
+
+    [Table("tb_carrinho")]
+    public class Carrinho
+    {
+        #region Atributos
+
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        public int Quantidade { get; set; }  
+
+        [ForeignKey("fk_cliente")]
+        public Usuario Cliente { get; set; }
+
+        [ForeignKey("fk_estoque")]
+        public Estoque Estoque { get; set; }
+
+        #endregion
+
+    }
+}
+
