503ab278e38725d197be15386e182b7d539579b0
Fernanda Chagas <fernanda.kmchagas@gmail.com>
2022-08-10 15:34:40 -0300

    @Fernanda-Chagas [Task:Assinaturas e Implementações adicionadas] {10/08/2022 15:34}

---
+++ EcoFarmAPI/Src/Repositorios/Implementacoes/EstoqueRepositorio.cs
@@ -0,0 +1,41 @@
+﻿using EcoFarmAPI.Src.Modelos;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace EcoFarmAPI.Src.Repositorios.Implementacoes
+{
+    /// <summary>
+    /// <para>Resumo: Classe responsavel por implementar IEstoque</para>
+    /// <para>Criado por: Fernanda</para>
+    /// <para>Versão: 1.0</para>
+    /// <para>Data: 10/08/2022</para>
+    /// </summary>
+
+    public class EstoqueRepositorio : IEstoque
+    {
+        public Task AtualizarPrudutoAsync(Estoque produto)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task DeletarProdutoAsync(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task NovoProdutoAsync(Estoque produto)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<Estoque> PegarProdutoPeloIdAsync(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<List<Estoque>> PegarTodosProdutosAsync()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
